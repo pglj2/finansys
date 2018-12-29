7d62be440b4a4a37d2fd1a14ad2ea01a5447fb67
pglj2 <pglj2@cin.ufpe.br>
2018-12-29 14:01:27 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,35 @@
+import { InMemoryDbService} from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService{
+    createDb(){
+        const categories = [
+            {
+                id:1,
+                name: "Lazer",
+                description: "cinema, parques, praia etc"
+            },
+            {
+                id:2,
+                name: "Saúde",
+                description: "Plano de saúde e remédios"
+            },
+            {
+                id:3,
+                name: "Linguagens de Programação",
+                description: "Java, C, C#, JavaScript e Haskell"
+            },
+            {
+                id:4,
+                name: "CIn",
+                description: "Ciência da computação, Engenharia da Computação e Sistemas da Informação"
+            },
+            {
+                id:5,
+                name: "UFPE",
+                description: "CIN, CAC, CTG, CCB, CFCH e CCSA"
+            }
+        ];
+        
+        return { categories }
+    }
+}
